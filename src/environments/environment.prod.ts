0889661cd06461033c481667a3dde2765c3931d5
Heike Winkelvoß <public@egladil.de>
2019-05-20 21:35:26 +0200

    prod-settings

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,11 @@
 export const environment = {
   production: true,
   version: '1.0.0',
-  envName: '',
-  apiUrl: 'https://unsichtbare-homepage/checklisten-api',
-  isDebugMode: false
+  envName: 'Production',
+  apiUrl: 'https://opa-wetterwachs.de/checklisten-api',
+  authUrl: 'https://opa-wetterwachs.de/auth-app',
+  isDebugMode: false,
+  clientId: '56qe22kr508u1yah2z90d8cbbn4hcb95okmntzck42kj',
+  signinRedirectUrl: 'https://opa-wetterwachs.de/checklistenapp',
+  loginRedirectUrl: 'https://opa-wetterwachs.de/checklistenapp/listen'
 };
