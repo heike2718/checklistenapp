8f4be6151c04998919c5a56c87158beb1744f229
Heike Winkelvoß <public@egladil.de>
2019-06-06 16:17:59 +0200

    dialogwidth gets released

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 
 export const environment = {
   production: false,
-  version: '1.1.2',
+  version: '1.1.3',
   envName: 'Development',
   apiUrl: 'http://localhost:9300/checklisten-api',
   authUrl: 'http://localhost:4300',
