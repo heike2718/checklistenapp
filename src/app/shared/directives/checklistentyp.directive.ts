e1500db285724e80c7e3c22acf54f45fb1aa2b7a
Heike Winkelvoß <public@egladil.de>
2019-07-21 16:58:51 +0200

    switch to Renderer2

--- src/app/shared/directives/checklistentyp.directive.ts
+++ src/app/shared/directives/checklistentyp.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, Input, ElementRef, Renderer, AfterViewInit } from '@angular/core';
+import { Directive, Input, ElementRef, AfterViewInit, Renderer2 } from '@angular/core';
 import { getBackgroundColorByChecklistentyp } from '../utils/checkliste.utils';
 
 @Directive({
@@ -9,10 +9,10 @@ export class ChecklistentypBackgroundDirective implements AfterViewInit {
 	@Input()
 	typ: string;
 
-	constructor(private el: ElementRef, private renderer: Renderer) { }
+	constructor(private el: ElementRef, private renderer: Renderer2) { }
 
 	ngAfterViewInit(): void {
 		const color = getBackgroundColorByChecklistentyp(this.typ);
-		this.renderer.setElementStyle(this.el.nativeElement, 'backgroundColor', color);
+		this.renderer.setStyle(this.el.nativeElement, 'backgroundColor', color);
 	}
 }
