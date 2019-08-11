e3ea435978501e4ba5db4d335755b1a0abcd9ded
Heike Winkelvoß <public@egladil.de>
2019-08-11 18:48:56 +0200

    release-4.0.0

--- src/environments/environment.qs.ts
+++ src/environments/environment.qs.ts
@@ -5,7 +5,7 @@
 
 export const environment = {
 	production: false,
-	version: '3.0.0',
+	version: '4.0.0',
 	envName: 'Test',
 	apiUrl: 'http://localhost:9300/checklisten-api',
 	authApiUrl: 'http://localhost:9000/authprovider',
