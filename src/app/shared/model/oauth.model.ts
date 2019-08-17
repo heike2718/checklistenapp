69a2e5d67dbcc9f322da63730419f7dee689581c
Heike Winkelvoß <public@egladil.de>
2019-08-17 10:13:30 +0200

    fixed 401 when an expired jwt is present

--- src/app/shared/model/oauth.model.ts
+++ src/app/shared/model/oauth.model.ts
@@ -1,5 +1,6 @@
-export const STORAGE_KEY_CLIENT_EXPIRES_AT = 'client_token_expires_at';
-export const STORAGE_KEY_CLIENT_ACCESS_TOKEN = 'client_access_token';
+export const SUFFIX_KEY_CLIENT_ACCESS_TOKEN = 'client_access_token';
+export const STORAGE_KEY_CLIENT_EXPIRES_AT = 'chl_client_token_expires_at';
+export const STORAGE_KEY_CLIENT_ACCESS_TOKEN = 'chl_client_access_token';
 
 export interface OAuthAccessTokenPayload {
 	accessToken: string;
@@ -7,7 +8,7 @@ export interface OAuthAccessTokenPayload {
 }
 
 export interface RefreshAccessTokenPayload {
-	clientAccessToken: string;
+	clientAccessToken: string[];
 	userRefreshToken: string;
 }
 
