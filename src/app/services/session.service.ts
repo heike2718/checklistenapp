cb5088f17188f603301bb65945cc7fd8f8b5c695
Heike Winkelvoß <public@egladil.de>
2019-09-08 19:32:31 +0200

    no refresh on sleep, serverside error logs

--- src/app/services/session.service.ts
+++ src/app/services/session.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { STORAGE_KEY_JWT_REFRESH_TOKEN, STORAGE_KEY_JWT, STORAGE_KEY_JWT_EXPIRES_AT, STORAGE_KEY_JWT_STATE } from 'hewi-ng-lib';
 import { Router } from '@angular/router';
+import { STORAGE_KEY_CLIENT_ACCESS_TOKEN } from '../shared/model/oauth.model';
 
 @Injectable({
 	providedIn: 'root'
@@ -18,5 +19,14 @@ export class SessionService {
 		this.router.navigateByUrl('/home');
 	}
 
+	getClientAccessToken(): string {
 
+		let at = localStorage.getItem(STORAGE_KEY_CLIENT_ACCESS_TOKEN);
+		if (!at) {
+			at = 'unknown - checklistenapp';
+		}
+
+		return at;
+
+	}
 }
