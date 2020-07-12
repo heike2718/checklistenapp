5cde12aa3e60749091260fc2cb5436705a8b0baf
Heike Winkelvoß <public@egladil.de>
2020-07-12 14:41:22 +0200

    versionsnr und releasenotes

--- src/app/shared/directives/auto-fokus.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { AutoFokusDirective } from './auto-fokus.directive';
-
-describe('AutoFokusDirective', () => {
-  it('should create an instance', () => {
-    const directive = new AutoFokusDirective();
-    expect(directive).toBeTruthy();
-  });
-});
